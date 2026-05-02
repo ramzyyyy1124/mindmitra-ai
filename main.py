e23b46cff0124e82f2460fd115d952b1c25a5937
Ramya Pathak <ramyapathak11@gmail.com>
2026-05-02 18:22:06 +0530

    Fix syntax error in main.py

--- main.py
+++ main.py
@@ -46,6 +46,7 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
+    )
     return {"access_token": access_token, "token_type": "bearer"}
 
 # --- Child Routes ---
